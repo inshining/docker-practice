72033a84a10fec7a5c498af30f6f771f73a5ed93
In Yeob, Kim <inshining.kim@gmail.com>
2022-06-26 23:08:06 +0900

    test case 수정

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders learn react link', () => {
+  const {getByText} = render(<App />);
+  const linkElement = getByText("안녕하세요!");
+  expect(linkElement).toBeInTheDocument();
+});
